79e3ee5170a6d536529ef69f35016cc43acd72cb
AshiNadekar <156462013+AshiNadekar@users.noreply.github.com>
2025-03-08 22:57:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,23 +13,26 @@ def save_uploaded_file(uploaded_file):
     with open(file_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
     return file_path
-
-if uploaded_file:
+if "button_pressed" not in st.session_state:
+    st.session_state.button_pressed=False
+if uploaded_file or st.session_state.button_pressed:
     saved_video_path = save_uploaded_file(uploaded_file)
 
 
 
-    if st.button("Process Video"):
+    if st.button("Process Video") or st.session_state.button_pressed:
         text=st.empty()
         text.write("Processing Video...")
         progress=st.progress(0)
-        saving_annotated_video(saved_video_path,progress_bar=progress)
+        if st.session_state.button_pressed==False:
+            saving_annotated_video(saved_video_path,progress_bar=progress)
+        st.session_state.button_pressed=True
         text.write("Completed..")
         st.success("Video processed successfully!")
+
+        x1=st.empty()
         with open('out.mp4', "rb") as f:
-            st.download_button("Download Anotated Video", f, file_name="processed_video.mp4", mime="video/mp4")
-
+            x1.download_button("Download Anotated Video", f, file_name="processed_video.mp4", mime="video/mp4")
+        x2=st.empty()
         with open('out.csv', "rb") as f:
-            st.download_button("Download CSV", f, file_name="data.csv", mime="text/csv")
-
-
+            x2.download_button("Download CSV", f, file_name="data.csv", mime="text/csv")
