f51fe94453b84984ffb8ac9ff98f141a3285cfdf
AshiNadekar <156462013+AshiNadekar@users.noreply.github.com>
2025-03-13 18:27:14 +0530

    Update shoplifting_detection.py

--- shoplifting_detection.py
+++ shoplifting_detection.py
@@ -1,3 +1,18 @@
 from inference_sdk import InferenceHTTPClient
 import supervision as sv
+CLIENT = InferenceHTTPClient(
+    api_url="https://detect.roboflow.com",
+    api_key=st.secrets["ROBOFLOW_API_KEY"]
+)
+def shoplifting_classifier(img):
+
+    result = CLIENT.infer(img, model_id="shoplifting-detection-erald/2")
+    return result
+def output(img):
+  result=extract_person_coordinate(img)
+  detections=sv.Detections.from_inference(result)
+  labels=detections.class_id
+  prob=detections.confidence
+  print(labels,prob)
+  
 
